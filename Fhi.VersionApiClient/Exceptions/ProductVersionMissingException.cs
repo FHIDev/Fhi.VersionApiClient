e20dfa684c1ed0030e0ec794d89d8df8cb3123be
GeirMG <geir.marius.gjul@fhi.no>
2024-11-15 14:27:56 +0100

    Fix last warning and set treatwarningaserror to true (#6)
    * Upgrade to .NET8
    
    * Bump Microsoft.Extensions.Configuration.Binder from 8.0.2 to 9.0.0
    
    * Bump Microsoft.Extensions.Logging.Abstractions from 8.0.2 to 9.0.0
    
    * Bump Microsoft.Extensions.Hosting.Abstractions from 8.0.1 to 9.0.0
    
    * Fix last warning and set treatwarningsaserrors to true. Also added Directory.Build.props and .editorconfig

---
+++ Fhi.VersionApiClient/Exceptions/ProductVersionMissingException.cs
@@ -0,0 +1,9 @@
+﻿namespace Fhi.Common.VersionApiClient.Exceptions
+{
+    public class ProductVersionMissingException : Exception
+    {
+        public ProductVersionMissingException() { }
+        public ProductVersionMissingException(string message) : base(message) { }
+        public ProductVersionMissingException(string message, Exception inner) : base(message, inner) { }
+    }
+}
